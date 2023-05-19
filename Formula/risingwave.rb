21f805ebaa6edceacdd1c03af4cae3c7a0ca0e89
Bugen Zhao <i@bugenzhao.com>
2023-05-19 18:21:55 +0800

    try try
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- Formula/risingwave.rb
+++ Formula/risingwave.rb
@@ -7,17 +7,18 @@ class Risingwave < Formula
   head "https://github.com/risingwavelabs/risingwave.git", branch: "main"
 
   depends_on "cmake" => :build
-  depends_on "llvm" => :build
   depends_on "protobuf" => :build
   depends_on "rustup-init" => :build
   depends_on "xz"
+  uses_from_macos "llvm" => :build
 
   def install
     # this will install the necessary cargo/rustup toolchain bits in HOMEBREW_CACHE
-    system Formula["rustup-init"].bin/"rustup-init", "-qy", "--no-modify-path", "--default-toolchain", "none"
-    ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
+    system "#{Formula["rustup-init"].bin}/rustup-init",
+           "-qy", "--no-modify-path",
+           "--default-toolchain", "none"
 
-    system "cargo", "install",
+    system HOMEBREW_CACHE/"cargo_cache/bin/cargo", "install",
            "--bin", "risingwave",
            "--features", "rw-static-link",
            *std_cargo_args(prefix, "src/cmd_all")
