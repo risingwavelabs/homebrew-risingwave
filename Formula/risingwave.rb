558fed6534f66bc9588556e2ee965f87287e1128
Bugen Zhao <i@bugenzhao.com>
2023-05-29 18:10:56 +0800

    use tap-new templates
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- Formula/risingwave.rb
+++ Formula/risingwave.rb
@@ -1,8 +1,9 @@
 class Risingwave < Formula
+  RW_VERSION = "0.19.0-alpha.1"
   desc "Distributed SQL database for stream processing"
   homepage "https://github.com/risingwavelabs/risingwave"
-  url "https://github.com/risingwavelabs/risingwave/archive/refs/tags/v0.19.0-alpha.1.tar.gz"
-  # version "0.19.0-xx"
+  url "https://github.com/risingwavelabs/risingwave/archive/refs/tags/v#{RW_VERSION}.tar.gz"
+  # version RW_VERSION
   sha256 "de3b3c4db3c9f8fc632abd93b5ef156061da4beb9dcc52988e839b2b129dc7cb"
   license "Apache-2.0"
   head "https://github.com/risingwavelabs/risingwave.git", branch: "main"
