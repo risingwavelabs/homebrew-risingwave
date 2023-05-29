cc778cd08fa92f664f7fcd79b431a71600f70219
Bugen Zhao <i@bugenzhao.com>
2023-05-29 19:06:13 +0800

    try again
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- Formula/risingwave.rb
+++ Formula/risingwave.rb
@@ -1,9 +1,7 @@
 class Risingwave < Formula
-  RW_VERSION = "0.19.0-alpha.1".freeze
   desc "Distributed SQL database for stream processing"
   homepage "https://github.com/risingwavelabs/risingwave"
-  url "https://github.com/risingwavelabs/risingwave/archive/refs/tags/v#{RW_VERSION}.tar.gz"
-  # version RW_VERSION
+  url "https://github.com/risingwavelabs/risingwave/archive/refs/tags/v0.19.0-alpha.1.tar.gz"
   sha256 "de3b3c4db3c9f8fc632abd93b5ef156061da4beb9dcc52988e839b2b129dc7cb"
   license "Apache-2.0"
   head "https://github.com/risingwavelabs/risingwave.git", branch: "main"
@@ -28,6 +26,6 @@ class Risingwave < Formula
   end
 
   test do
-    system "#{bin}/risingwave", "frontend", "--help"
+    system "#{bin}/risingwave", "--help"
   end
 end
