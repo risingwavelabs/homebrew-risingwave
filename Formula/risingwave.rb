f686cfaa7196ca037b4675165bfc19df895f654a
Bugen Zhao <i@bugenzhao.com>
2023-06-01 17:01:29 +0800

    bump version to 0.19.0
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- Formula/risingwave.rb
+++ Formula/risingwave.rb
@@ -1,18 +1,11 @@
 class Risingwave < Formula
-  RW_VERSION = "0.19.0-alpha.1".freeze
+  RW_VERSION = "0.19.0".freeze
   desc "Distributed SQL database for stream processing"
   homepage "https://github.com/risingwavelabs/risingwave"
   url "https://github.com/risingwavelabs/risingwave/archive/refs/tags/v#{RW_VERSION}.tar.gz"
-  sha256 "de3b3c4db3c9f8fc632abd93b5ef156061da4beb9dcc52988e839b2b129dc7cb"
+  sha256 "b6a35dd32b773ab2405372286329b31b43cc86d7e7dd901f27c7b14de5a5a42d"
   license "Apache-2.0"
   head "https://github.com/risingwavelabs/risingwave.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/risingwavelabs/homebrew-risingwave/releases/download/risingwave-0.19.0-alpha.1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e0726cd4d83fc48f1f0362c056d859918b4b295a957297a225911777c8cf9e04"
-    sha256                               monterey:      "96b020ddcde6363e9a92e59ca207259f3477547621a04522b47305a4f809426e"
-  end
 
   depends_on "cmake" => :build
   depends_on "llvm" => :build
