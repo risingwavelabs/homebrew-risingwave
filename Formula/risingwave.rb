ff1279bec4831f29a56ee5442caca72e4a188a2e
xxchan <xxchan22f@gmail.com>
2023-09-19 08:35:21 +0800

    bump

--- Formula/risingwave.rb
+++ Formula/risingwave.rb
@@ -1,17 +1,11 @@
 class Risingwave < Formula
-  RW_VERSION = "1.1.2".freeze
+  RW_VERSION = "1.2.0".freeze
   desc "Distributed SQL database for stream processing"
   homepage "https://github.com/risingwavelabs/risingwave"
   url "https://github.com/risingwavelabs/risingwave/archive/refs/tags/v#{RW_VERSION}.tar.gz"
-  sha256 "9f137bded84d3fe7713c4bc37674c40f6a7f1c73ef68df55a6e71f1656af5231"
+  sha256 "8dc4e3997941a31b3ff08ac0be760cbdcb840b8e40dcc99b40c2eafe346e034c"
   license "Apache-2.0"
   head "https://github.com/risingwavelabs/risingwave.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/risingwavelabs/homebrew-risingwave/releases/download/risingwave-1.1.2"
-    sha256 cellar: :any, arm64_monterey: "bc4e9742da58038881a106b2f4000f94d5ca94da96451d1b68d386f60c028894"
-    sha256 cellar: :any, monterey:       "a23810510625b3c83fb61de6645e6b2d15c567ce1c5914ca6a872cd46aab83bc"
-  end
 
   depends_on "cmake" => :build
   depends_on "llvm" => :build
