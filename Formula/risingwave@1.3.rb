2d46b2731df18bb29a79a5d20b9e4dd7c2db3131
xxchan <xxchan22f@gmail.com>
2023-11-10 11:36:36 +0800

    add 1.3
    Closes #18.
    
    Signed-off-by: xxchan <37948597+xxchan@users.noreply.github.com>

---
+++ Formula/risingwave@1.3.rb
@@ -0,0 +1,46 @@
+class RisingwaveAT13 < Formula
+  desc "Distributed SQL database for stream processing"
+  homepage "https://github.com/risingwavelabs/risingwave"
+  url "https://github.com/risingwavelabs/risingwave/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "a0109dcf4e6909bcf25ac2d65c33bfe7c81facdbf1f8e8add7c6eab09006cbe1"
+  license "Apache-2.0"
+  head "https://github.com/risingwavelabs/risingwave.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/risingwavelabs/homebrew-risingwave/releases/download/risingwave-1.3.0"
+    sha256 cellar: :any, arm64_ventura: "abaeb7b147e61ebc636de938d5e4cd7ea19cf0098c2487c6ae734108994d5c10"
+    sha256 cellar: :any, ventura:       "d33f51a0ae0365716b70c08fc536922cadd2c204e628a8ec8ed9602e3696de3b"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "llvm" => :build
+  depends_on "protobuf" => :build
+  depends_on "rustup-init" => :build
+  depends_on "openssl@3"
+  depends_on "xz"
+
+  def install
+    # this will install the necessary cargo/rustup toolchain bits in HOMEBREW_CACHE
+    system "#{Formula["rustup-init"].bin}/rustup-init",
+           "-qy", "--no-modify-path",
+           "--default-toolchain", "none"
+    ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
+
+    ENV.delete "RUSTFLAGS" # https://github.com/Homebrew/brew/pull/15544#issuecomment-1628639703
+    # Homebrew changes cxx flags, and CMake doesn't pick them up, so rdkafka-sys build fails.
+    # We cannot pass CMake flags (`std_cmake_args`) because it's in their build.rs.
+    #
+    # Some refs that might be useful:
+    # https://github.com/Homebrew/homebrew-core/pull/51949#issuecomment-601943075
+    # https://github.com/Homebrew/brew/pull/7134
+    ENV["SDKROOT"] = MacOS.sdk_path_if_needed
+    system "cargo", "install",
+           "--bin", "risingwave",
+           "--features", "rw-static-link",
+           *std_cargo_args(path: "src/cmd_all") # "--locked", "--root ...", "--path src/cmd_all"
+  end
+
+  test do
+    system "#{bin}/risingwave", "--help"
+  end
+end
