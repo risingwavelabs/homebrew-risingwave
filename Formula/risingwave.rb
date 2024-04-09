b0821341175181ff3dab852aae89f3f2d808a796
Bugen Zhao <i@bugenzhao.com>
2024-04-09 13:37:10 +0800

    Revert "risingwave 1.8.0 (#34)"
    This reverts commit 51a355169cd3afed374c9cf9df2c0393513be774.

--- Formula/risingwave.rb
+++ Formula/risingwave.rb
@@ -1,9 +1,10 @@
 class Risingwave < Formula
   desc "Distributed SQL database for stream processing"
   homepage "https://github.com/risingwavelabs/risingwave"
-  url "https://github.com/risingwavelabs/risingwave/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "b0d58a749b3f6eb375ddee232fb6954ad110c958420dbbfb0e7db76416778668"
+  url "https://github.com/risingwavelabs/risingwave/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "139d9b0947618ce711000b0a22bb05a0e3ce90f103e8f2f7e573dee485c2a6ad"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/risingwavelabs/risingwave.git", branch: "main"
 
   bottle do
@@ -18,10 +19,11 @@ class Risingwave < Formula
   depends_on "rustup-init" => :build
   depends_on "java11"
   depends_on "openssl@3"
+  depends_on "xz"
 
   resource "connector" do
-    url "https://github.com/risingwavelabs/risingwave/releases/download/v1.8.0/risingwave-v1.8.0-x86_64-unknown-linux-all-in-one.tar.gz"
-    sha256 "341fd43fe75535732e67f11dee544cf309b30a30ad76370a6d5313dc6a5147e5"
+    url "https://github.com/risingwavelabs/risingwave/releases/download/v1.7.0/risingwave-v1.7.0-x86_64-unknown-linux-all-in-one.tar.gz"
+    sha256 "aca4affd6fadf33b927d4945d9bfffb73ed4af528aa99b758903600352ac1321"
   end
 
   def install
